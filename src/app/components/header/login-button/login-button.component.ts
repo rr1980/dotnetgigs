c600a473a3e1aad7029f5ebfe33f4f92f2e1623b
rr1980 <rener1980@googlemail.com>
2017-07-15 18:28:41 +0200

    a

--- src/app/components/header/login-button/login-button.component.ts
+++ src/app/components/header/login-button/login-button.component.ts
@@ -2,21 +2,20 @@ import { Component } from '@angular/core';
 import { AuthGuard } from '../../../services/auth.guard';
 
 @Component({
-    moduleId: module.id,
-    selector: 'login-button',
-    templateUrl: 'login-button.component.html',
-    styleUrls: ['login-button.component.scss']
+  moduleId: module.id,
+  selector: 'login-button',
+  templateUrl: 'login-button.component.html',
+  styleUrls: ['login-button.component.scss']
 })
 export class LoginButtonComponent {
 
-    constructor(private authGuard: AuthGuard) { }
+  constructor(private authGuard: AuthGuard) { }
 
-    logout(event) {
-        this.authGuard.Logout();
-    }
+  logout(event) {
+    this.authGuard.Logout();
+  }
 
-    public IsLoggedIn(): boolean {
-        return this.authGuard.IsLoggedIn();
-    }
-
+  public IsLoggedIn(): boolean {
+    return this.authGuard.IsLoggedIn();
+  }
 }
